f20c8956a31026b39ec36a2d361a52560253a7ee
Aroita <aroitaim84@gmail.com>
2022-06-13 14:01:01 +0200

    componente-cli

--- mi-primera-app/src/app/app.module.ts
+++ mi-primera-app/src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { personasComponent } from './personas/personas.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    personasComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule
