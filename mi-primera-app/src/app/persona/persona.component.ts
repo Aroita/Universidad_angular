af91b2d50673c2673fbcaf9f79ead01821e73663
Aroita <aroitaim84@gmail.com>
2022-06-21 17:54:46 +0200

    seccion_3

--- mi-primera-app/src/app/persona/persona.component.ts
+++ mi-primera-app/src/app/persona/persona.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonaComponent {
 
-  constructor() { }
+  nombre: string = 'Aroa';
+  apellido: string = 'Irmo';
+  private edad: number = 28;
+
+  getEdad():number{
+    return this.edad;
+  }
 
 }
