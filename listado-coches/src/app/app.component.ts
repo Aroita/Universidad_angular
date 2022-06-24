5ec717b1daad25b92de3e9b1a96d06ca6e72e0ea
Aroita <aroitaim84@gmail.com>
2022-06-24 14:23:17 +0200

    seccion_4_directivas ngIf y ngForm

--- listado-coches/src/app/app.component.ts
+++ listado-coches/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Coche } from './coche.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listado-coches';
+  titulo = 'Lista de coches en ventas';
+  coches: Coche[] = [
+    new Coche('BMW', 'Serie3'), 
+    new Coche('Audi', 'A4')
+  ];
+  cocheInput: string = '';
+  modeloInput: string = '';
+
+  agregarCoche(){
+    let coche1 = new Coche(this.cocheInput, this.modeloInput);
+    this.coches.push(coche1)
+    
+  }
 }
